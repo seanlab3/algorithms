7274d93df1b4e33bcafa671bab36899bb6c1f702
seanlab1 <seanlab1@seanlabcoding33.onmicrosoft.com>
2020-10-05 11:41:50 +0900

    hacktoberfest-accepted

--- algorithms_practice/1.arrays/4.Arrays_Josephus_problem.py
+++ algorithms_practice/1.arrays/4.Arrays_Josephus_problem.py
@@ -7,6 +7,19 @@ For example:
 Input: consider 123456789 members sitting in a circular fashion,
 Output: 369485271
 """
+def josephus(int_list, skip):
+    skip = skip - 1                     # list starts with 0 index
+    idx = 0
+    len_list = (len(int_list))
+    while len_list > 0:
+        idx = (skip + idx) % len_list   # hash index to every 3rd
+        yield int_list.pop(idx)
+        len_list -= 1
+a=[1,2,3,4,5,6,7,8,9]
+k=3
+
+for i in josephus(a,k):
+    print(i)
 from algorithms.arrays import josephus
 
 a=[1,2,3,4,5,6,7,8,9]
